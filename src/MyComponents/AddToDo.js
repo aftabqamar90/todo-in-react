8151923a32812bb335fed567d72b1e31866ac6a7
aftabqamar90 <67581271+aftabqamar90@users.noreply.github.com>
2023-04-09 12:56:12 +0500

    Add with routing

--- src/MyComponents/AddToDo.js
+++ src/MyComponents/AddToDo.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
+export default function AddToDo(props) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const submit = (e) => {
+        e.preventDefault();
+        if (!title || !description) {
+            alert("Title or Description cannot be blank");
+        }
+        else {
+            setTitle("");
+            setDescription("");
+            props.addTodo(title, description);
+        }
+    }
 
-export default function AddToDo() {
     return (
         <div>
-            <form>
+            <form onSubmit={submit}>
                 <div className="mb-3">
-                    <input type="text" placeholder='Title' className="form-control" id="Title" name="Title" aria-describedby="emailHelp" />
+                    <input type="text" placeholder='Title' className="form-control" value={title} onChange={(e) => setTitle(e.target
+                        .value)} id="Title" name="Title" />
                 </div>
                 <div className="mb-3">
-                    <input type="text" className="form-control" id="Description" placeholder='Description'/>
+                    <input type="text" onChange={(e) => setDescription(e.target.value)} className="form-control" id="Description" value={description} placeholder='Description' />
                 </div>
-                <button type="button" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
     )
